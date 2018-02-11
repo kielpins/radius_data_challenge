ac87615c8f8cbbc06fb27c6fb2777bf9860cf95f
Dave Kielpinski <dave.kielpinski@gmail.com>
2018-02-10 20:14:41 -0800

    add filters for fields
    added filters for address, city, revenue
    category code filter is in process

--- radius.py
+++ radius.py
@@ -1,87 +1,32 @@
-import os, json, re, copy
+import os, json, copy
 import pandas as pd
+import validate
 
-raw_data_file = r'/Users/dave/Documents/GitHub/radius/data_analysis.json'
+reload_data = True
+# load Radius data
+if reload_data:
+    raw_data_file = r'/Users/dave/Documents/GitHub/radius/data_analysis.json'
+    with open(raw_data_file) as fp:
+        raw_data = json.load(fp)
+        data = pd.DataFrame.from_records(raw_data)
 
-with open(raw_data_file) as fp:
-    raw_data = json.load(fp)
-
-data = pd.DataFrame.from_records(raw_data)
-
-# address question 1
+# answer question 1
 print('Number of non-null records in each field:')
 print(data.count(axis=0))
 
-# address question 2
-# define filtering for the various fields
-
-# regex looks for basic (123) 456-7890 format with variations in use of parentheses, dashes, and whitespace 
-phone_regex_string = '\(*\s*([0-9]{3})\s*\)*\-*\s*\-*([0-9]{3})\s*\-*\s*([0-9]{4})'
-# compile regex for efficiency since we will use it many times
-phone_re = re.compile(phone_regex_string)
-
-def find_bad_phone(val):
-    if not pd.isnull(val):
-        try:
-            if not str.isnumeric(val):
-                m = re.match(phone_re, val)
-                if m is None:
-                    return val
-        except TypeError:
-            return val
-    else:
-        return None
-
-def is_valid_phone(val):
-    if pd.isnull(val):
-        return False
-    if type(val) is int:
-        val = str(val)
-    elif not str.isnumeric(val): # use regex to extract numeric string from value
-        m = re.match(phone_re, val)
-        try:
-            if len(m.groups()) == 4:
-                val = ''.join([g for g in m.groups()[1:]])
-        except AttributeError:
-            return False
-    if len(val) == 10: # phone number should have 10 digits
-        return True
-    else:
-        return False
-
-def find_bad_zip(val):
-    if pd.isnull(val):
-        return None
-    if type(val) is int:
-        val = str(val)
-    if str.isnumeric(val) and len(val) != 5:
-        return val
-    return None
-        
-def is_valid_zip(val):
-    if pd.isnull(val):
-        return False
-    if type(val) is int:
-        val = str(val)
-    if str.isnumeric(val) and len(val) == 5:
-        return True
-    else:
-        return False
-    
-is_valid = {
-    'phone': is_valid_phone,
-    'zip': is_valid_zip,
-}
-
+# answer question 2
 data_is_valid = copy.deepcopy(data)
-cols_to_validate = ['phone', 'zip'] 
+cols_to_validate = []
+#cols_to_validate = ['name', 'address', 'city', 'phone', 'revenue', 'state', 'zip',] 
+# note validation is actually performed by the function registry validate.is_valid
 for c in cols_to_validate:
-    data_is_valid[c] = data[c].apply(is_valid[c])
+    data_is_valid[c] = data[c].apply(validate.is_valid[c])
+print('Number of true-valued records:')
+print(data_is_valid[cols_to_validate].sum(axis=0).astype(int))
 
 
-phu = pd.Series(list(filter(check_phone, data['phone']))).unique()
+rv = pd.Series(list(filter(validate.find_bad_catcode, data['category_code']))).unique()
 
-
-# address question 3
+# answer question 3
 print('Cardinality of')
-    
+
--- validate.py
+++ validate.py
@@ -1,4 +1,105 @@
+import re
 import pandas as pd
+
+# load US geographical data, sourced from GeoNames.org
+# Puerto Rico and Virgin Islands are referenced in data, make sure these are appended
+col_names = ['zip', 'city', 'state', 'state_abbrev']
+us_geonames = pd.read_csv('US.txt', sep='\t', usecols=[1,2,3,4], names=col_names)
+puerto_rico_geonames = pd.read_csv('PR.txt', sep='\t', usecols=[1,2,3,4], names=col_names)
+virgin_islands_geonames = pd.read_csv('VI.txt', sep='\t', usecols=[1,2,3,4], names=col_names)
+us_geonames = us_geonames.append(puerto_rico_geonames).append(virgin_islands_geonames)
+
+## filter for valid address
+
+address_stemmer_string = '([0-9a-z ]*) ste'
+address_stemmer_re = re.compile(address_stemmer_string)
+
+def find_bad_address(val):
+    if pd.isnull(val):
+       return None
+    if type(val) is not str:
+        return val
+    if len(val) < 1:
+        return val
+    if not str.isnumeric(val[0]):
+        return val
+    return None
+
+def is_valid_address(val):
+    if pd.isnull(val):
+        return False
+    if type(val) is not str:
+        return False
+    if len(val) < 1:
+        return False
+    if not str.isnumeric(val[0]):
+        return False
+    return True
+
+## filter for valid category code
+
+catvals = None
+
+def find_bad_catcode(val):
+    if pd.isnull(val):
+        return None
+    val = str(val)
+    if len(val) < 8:
+        return val
+    else:
+        val = val[:-2]
+        if val not in catvals:
+            return val
+    return None
+
+def is_valid_catcode(val):
+    if pd.isnull(val):
+        return False
+    if type(val) is not str:
+        return False
+    if val not in catvals:
+        return False
+    return True
+
+## filter for valid cities
+    
+cityvals = set(list(map(lambda x: x.lower(), us_geonames['city'].values)))
+
+def find_bad_city(val):
+    if pd.isnull(val):
+        return None
+    if type(val) is not str:
+        return val
+    if val.lower() not in cityvals:
+        return val
+    return None
+
+def is_valid_city(val):
+    if pd.isnull(val):
+        return False
+    if type(val) is not str:
+        return False
+    if val.lower() not in cityvals:
+        return False
+    return True
+
+## filter for valid names
+
+def find_bad_name(val):
+    if pd.isnull(val):
+        return None
+    if type(val) is not str:
+        return val
+    return None
+
+def is_valid_name(val):
+    if pd.isnull(val):
+        return False
+    if type(val) is not str:
+        return False
+    return True
+
+## filter for valid phone numbers
 
 # regex looks for basic (123) 456-7890 format with variations in use of parentheses, dashes, and whitespace 
 phone_regex_string = '\(*\s*([0-9]{3})\s*\)*\-*\s*\-*([0-9]{3})\s*\-*\s*([0-9]{4})'
@@ -34,6 +135,66 @@ def is_valid_phone(val):
     else:
         return False
 
+## filter for valid revenue
+
+revenue_regex_string = '\$*([0-9.]*\,*[0-9]+) to \$*([0-9.]+) million'
+revenue_re = re.compile(revenue_regex_string)
+revenue_special_cases = ['less than $500,000', 'over $1 billion', 'over $500 million']
+
+def find_bad_revenue(val):
+    if pd.isnull(val):
+       return None
+    if type(val) is str:
+        m = re.match(revenue_re, val.lower())
+        if m is None:
+            for rsc in revenue_special_cases:
+                if val.lower() == rsc:
+                    return None
+            return val
+    else:
+        return val
+
+def is_valid_revenue(val):
+    if pd.isnull(val):
+        return False
+    if type(val) is str:
+        m = re.match(revenue_re, val.lower())
+        if m is not None:
+            return True
+        else:
+            for rsc in revenue_special_cases:
+                if val.lower() == rsc:
+                    return True
+            return False
+    return False
+        
+
+## filter for valid states
+    
+statevals = set(us_geonames['state_abbrev'].values)
+
+def find_bad_state(val):
+    if pd.isnull(val):
+        return None
+    if type(val) is not str:
+        return val
+    if val not in statevals:
+        return val
+    return None
+
+def is_valid_state(val):
+    if pd.isnull(val):
+        return False
+    if type(val) is not str:
+        return False
+    if val not in statevals:
+        return False
+    return True
+
+## filter for valid zip codes
+    
+zipvals = set(us_geonames['zip'].astype(int).values)
+
 def find_bad_zip(val):
     if pd.isnull(val):
         return None
@@ -49,11 +210,16 @@ def is_valid_zip(val):
     if type(val) is int:
         val = str(val)
     if str.isnumeric(val) and len(val) == 5:
-        return True
+        if int(val) in zipvals:
+            return True
     else:
         return False
-    
+
 is_valid = {
+    'city': is_valid_city,
+    'name': is_valid_name,
     'phone': is_valid_phone,
+    'revenue': is_valid_revenue,
+    'state': is_valid_state,
     'zip': is_valid_zip,
 }
